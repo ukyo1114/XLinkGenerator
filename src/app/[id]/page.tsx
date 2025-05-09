ac8928929f71c0c3969a32b2330283c3335ddb70
ukyo1114 <xjr1300sunny@gmail.com>
2025-05-09 22:04:58 +0900

    refactor: simplify page component structure

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,24 +2,19 @@ import { fetchItem } from "@/util/fetchItem";
 import { redirect } from "next/navigation";
 import type { Metadata } from "next";
 
-interface PageProps {
-  params: Promise<{ id: string }>;
-  searchParams: { [key: string]: string | string[] | undefined };
-}
+type Props = {
+  params: { id: string };
+};
 
-export default async function RedirectPage({ params }: PageProps) {
-  const { id } = await params;
-  const data = await fetchItem(id);
+export default async function Page({ params }: Props) {
+  const data = await fetchItem(params.id);
   if (!data) redirect("/not-found");
 
   redirect(`https://x.com/${data.xAccount}`);
 }
 
-export async function generateMetadata({
-  params,
-}: PageProps): Promise<Metadata> {
-  const { id } = await params;
-  const data = await fetchItem(id);
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const data = await fetchItem(params.id);
   if (!data) return {};
 
   return {
